2ac6f4de0fb6e1c17e4acd1eb83cd9e3a67b7b8b
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-17 12:21:55 -0700

    Effie ordered a Spilt Volcano for $6.41.

--- app/orders.rb
+++ app/orders.rb
@@ -1,4 +1,10 @@
 class Order < ActiveRecord::Base
-  belongs_to :customers
+  belongs_to :customer
   belongs_to :coffee
+
+  def receipt
+    puts "#{self.customer.name} ordered a #{self.coffee.title} for #{self.price}."
+  end
+
+
 end
---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+Coffee.destroy_all
+Customer.destroy_all
+Order.destroy_all
+Coffee.reset_pk_sequence
+Customer.reset_pk_sequence
+Order.reset_pk_sequence
+
+Faker::UniqueGenerator.clear
+
+puts "seeding coffee...."
+5.times { Coffee.create title: Faker::Coffee.unique.blend_name }
+
+puts "letting in customers...."
+20.times { Customer.create name: Faker::Name.unique.first_name } 
+
+puts "taking orders...."
+50.times { Order.create price: Faker::Number.between(from: 2.50, to: 8.99).round(2), customer: Customer.all.sample, coffee: Coffee.all.sample }
+
+puts "Order Up!"
